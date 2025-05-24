6fe7f5374839283bbdffddb1c95eb72c95e2792d
quickshopzone <abidazad0000@gmail.com>
2025-05-24 18:53:06 +0530

    Add files via upload

--- button.js
+++ button.js
@@ -1,2 +1,5 @@
-// Mock component for button.js
-export const Button = () => null;
+export const Button = ({ children, ...props }) => (
+  <button {...props} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+    {children}
+  </button>
+);
---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import QuickShopZone from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuickShopZone />
+  </React.StrictMode>
+);
